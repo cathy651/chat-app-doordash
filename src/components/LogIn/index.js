7d89cb0f8982a3f693e9ddd6ebcd51645bfa84a1
cathyliu651 <cathyweiyiliu@gmail.com>
2023-02-20 01:10:29 -0800

    functionally finished routers, no css yet

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -3,30 +3,40 @@ import AppContext from "../../contexts/AppContext";
 import './LogIn.css'
 
 const LogIn = () => {
-  const { userName, setUserName } = useContext(AppContext);
+  const { setUserName } = useContext(AppContext);
   const [ curInput, setCurInput ] = useState('');
+  const [ showLogIn, setShowLogIn ] = useState(true);
 
   const handleSubmit = () => {
     setUserName(curInput);
+    setShowLogIn(false);
   }
 
   return (
-    <div className='main-container'>
-      <form className='form-container'>
-        <input 
-          className='input-box' 
-          type="text" 
-          onChange={e => setCurInput(e.target.value)}
-          placeholder='Your Name'
-          name='name'
-        />
-      
-        <button 
-          className='btn'
-          onClick={handleSubmit}
-        >join us</button>
-      </form>
-    </div>
+    <>
+    {
+      showLogIn?
+      <div className='main-container'>
+        <form className='form-container'>
+          <input 
+            className='input-box' 
+            type="text" 
+            onChange={e => setCurInput(e.target.value)}
+            placeholder='Your Name'
+            name='name'
+          />
+        
+          <button 
+            className='btn'
+            onClick={handleSubmit}
+          >join us</button>
+        </form>
+      </div>
+      :
+      null
+    }
+    </>
+    
   )
 }
 
